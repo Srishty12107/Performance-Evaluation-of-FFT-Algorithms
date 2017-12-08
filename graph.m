670ce2c5ec8756a70e5447487fc8e81dc256ec73
Srishty12107 <Srishty12107@iiitd.ac.in>
2017-12-07 22:09:22 -0500

    plot graphs

---
+++ graph.m
@@ -0,0 +1,20 @@
+%fetches mat file and plot graphs
+plot(mse_fft(1:4));
+hold on;
+plot(mse_czt(1:4));
+hold on;
+plot(mse_radix_audio(1:4));
+title({'MSE of audio 1(Radix2 vs FFT vs CZT)'})
+xlabel('Frequency (hz)')
+ylabel('MSE')
+hold off;
+
+plot(SNR_czt_audio(1:4));
+hold on;
+plot(SNR_fft_audio(1:4));
+hold  on;
+plot(SNR_radix2_audio(1:4));
+title({'SNR of audio 1 signal (Radix 2 vs FFT vs CZT)'})
+xlabel('Frequency (Hz)')
+ylabel('SNR')
+hold off;
